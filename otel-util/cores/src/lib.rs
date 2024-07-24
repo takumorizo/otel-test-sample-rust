c8facb7e372886929e5d8a3222dcde4b18307c5b
takuya.moriyama <takuya.moriyama@agc.com>
2024-07-25 07:25:57 +0900

    add otel init

--- otel-util/cores/src/lib.rs
+++ otel-util/cores/src/lib.rs
@@ -3,7 +3,7 @@ use deps::opentelemetry::{global, propagation::Extractor, propagation::Injector}
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-// ブログにあった感じで、injector/extractor を実装してみる。
+// 参考記事に倣って、実装。
 // https://peterpaul.hashnode.dev/opentelemetry-context-propagation-with-rust
 // Serializable datastructure to hold the opentelemetry propagation context.
 #[derive(Debug, Clone, Serialize, Deserialize)]
@@ -41,5 +41,94 @@ impl Extractor for PropagationContext {
 
     fn keys(&self) -> Vec<&str> {
         self.0.keys().map(|k| k.as_ref()).collect()
+    }
+}
+
+use deps::opentelemetry::KeyValue;
+use deps::opentelemetry_otlp;
+use deps::opentelemetry_otlp::WithExportConfig;
+use deps::opentelemetry_sdk;
+use deps::opentelemetry_sdk::{
+    runtime,
+    trace::{BatchConfig, RandomIdGenerator, Sampler, Tracer},
+    Resource,
+};
+use deps::opentelemetry_semantic_conventions::{
+    resource::{DEPLOYMENT_ENVIRONMENT, SERVICE_NAME, SERVICE_VERSION},
+    SCHEMA_URL,
+};
+use deps::tokio::runtime::Handle;
+use deps::tracing;
+use deps::tracing_core::Level;
+use deps::tracing_opentelemetry::OpenTelemetryLayer;
+use deps::tracing_subscriber;
+use deps::tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+fn resource(service_name: &str, version: &str) -> Resource {
+    Resource::from_schema_url(
+        [
+            KeyValue::new(SERVICE_NAME, service_name.to_string()),
+            KeyValue::new(SERVICE_VERSION, version.to_string()),
+            KeyValue::new(
+                DEPLOYMENT_ENVIRONMENT,
+                std::env::var("DEPLOYMENT_ENVIRONMENT").unwrap_or("unknown".to_string()),
+            ),
+        ],
+        SCHEMA_URL,
+    )
+}
+
+fn init_tracer(collector_endpoint: &str, service_name: &str, version: &str) -> Tracer {
+    global::set_text_map_propagator(opentelemetry_sdk::propagation::TraceContextPropagator::new());
+
+    opentelemetry_otlp::new_pipeline()
+        .tracing()
+        .with_trace_config(
+            opentelemetry_sdk::trace::Config::default()
+                // Customize sampling strategy
+                .with_sampler(Sampler::ParentBased(Box::new(Sampler::TraceIdRatioBased(
+                    1.0,
+                ))))
+                // If export trace to AWS X-Ray, you can use XrayIdGenerator
+                .with_id_generator(RandomIdGenerator::default())
+                .with_resource(resource(service_name, version)),
+        )
+        .with_batch_config(BatchConfig::default())
+        .with_exporter(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint(collector_endpoint),
+        )
+        .install_batch(runtime::Tokio)
+        .unwrap()
+}
+
+pub fn init_telemetry(collector_endpoint: &str, service_name: &str, version: &str) -> OtelGuard {
+    let tracer = init_tracer(collector_endpoint, service_name, version);
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::filter::LevelFilter::from_level(
+            Level::INFO,
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .with(OpenTelemetryLayer::new(tracer.clone()))
+        .init();
+
+    std::panic::set_hook(Box::new(|panic_info| {
+        tracing::error!("panic occurred: {}", panic_info);
+    }));
+
+    OtelGuard { tracer }
+}
+
+pub struct OtelGuard {
+    tracer: Tracer,
+}
+
+impl Drop for OtelGuard {
+    fn drop(&mut self) {
+        Handle::current().spawn(async {
+            opentelemetry::global::shutdown_tracer_provider();
+        });
     }
 }
