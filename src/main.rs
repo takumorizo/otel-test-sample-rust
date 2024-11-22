7f0df11d90685196be257e310599bc238a95b571
takuya.moriyama <takuya.moriyama@agc.com>
2024-11-22 10:26:48 +0900

    test 関数が、error を返す場合に対応させるマクロを作成（要リファクタ）

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ fn main() {
 }
 
 use anyhow::{anyhow, Result};
-use otel_util::{tracing, use_otel_at_test};
+use otel_util::{tracing, use_otel_at_test, use_otel_at_test_error};
 
 #[tracing::instrument(err)]
 fn sample_add(a: u64, b: u64) -> Result<u64> {
@@ -40,21 +40,21 @@ async fn failed_otel_test() {
 }
 
 // TODO: Return type は、型がない場合にのみ、対応している。
-// #[use_otel_at_test]
-// async fn error_otel_test() -> anyhow::Result<()> {
-//     // given
-//     let a = 10;
-//     let b = 20;
+#[use_otel_at_test_error]
+async fn error_otel_test() -> anyhow::Result<()> {
+    // given
+    let a = 10;
+    let b = 20;
 
-//     // when
-//     tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-//     let c = sample_add_err(a, b)?;
+    // when
+    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+    let c = sample_add_err(a, b)?;
 
-//     // then
-//     assert_eq!(100, 10);
-//     assert_eq!(a + b, c);
-//     Ok::<(), anyhow::Error>(())
-// }
+    // then
+    assert_eq!(100, 10);
+    assert_eq!(a + b, c);
+    Ok::<(), anyhow::Error>(())
+}
 
 #[use_otel_at_test]
 async fn panic_otel_test() {
