43a2f56524a75a77c3e91ca3caadac0f447c9773
takuya.moriyama <takuya.moriyama@agc.com>
2024-11-22 11:25:47 +0900

    不要なコメントの削除。

--- otel-util/macros/src/lib.rs
+++ otel-util/macros/src/lib.rs
@@ -36,10 +36,6 @@ pub fn use_otel_at_test(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let fn_name = &input.sig.ident;
     let return_type = &input.sig.output;
     let is_default_type = *return_type == syn::ReturnType::Default;
-    // if !is_default_type {
-    //     panic!("use_otel_at_test_error macro if test function returns Error");
-    // }
-    // assert_eq!(*return_type, syn::ReturnType::Default);
     let block = &input.block;
 
     let args = parse_macro_input!(_attr as AttributeArgs);
@@ -94,68 +90,3 @@ pub fn use_otel_at_test(_attr: TokenStream, item: TokenStream) -> TokenStream {
     };
     TokenStream::from(expanded)
 }
-
-// #[proc_macro_attribute]
-// pub fn use_otel_at_test_error(_attr: TokenStream, item: TokenStream) -> TokenStream {
-//     let input = parse_macro_input!(item as ItemFn);
-//     let fn_name = &input.sig.ident;
-//     let return_type = &input.sig.output;
-//     let is_default_type = *return_type == syn::ReturnType::Default;
-//     if is_default_type {
-//         panic!("use_otel_at_test macro if test function has not return type");
-//     }
-//     // assert_eq!(*return_type, syn::ReturnType::Default);
-//     let block = &input.block;
-
-//     let args = parse_macro_input!(_attr as AttributeArgs);
-//     let my_args = UseOtelTestArgs::new(args);
-//     let (endpoint, other_args) = (my_args.endpoint, my_args.others);
-
-//     let tokio_test_attrs = match other_args.len() {
-//         0 => quote! { #[tokio::test] },
-//         _ => {
-//             quote! { #[tokio::test(#(#other_args),*)] }
-//         }
-//     };
-
-//     if !other_args.is_empty() {
-//         println!(
-//             "Warning: Other macro args except for endpoint is depracted in use_otel_at_test macro, function name = {}, other macro args = {:?}",
-//             fn_name, other_args
-//         );
-//     }
-
-//     let expanded = quote! {
-//         #tokio_test_attrs
-//         async fn #fn_name() {
-//             let __otel_guard_for_otel_test;
-//             {
-//                 use otel_util::DefaultSimpleOtelGuardFactory;
-//                 __otel_guard_for_otel_test = DefaultSimpleOtelGuardFactory::new(#endpoint, stringify!(#fn_name), "non-deployment").build();
-//             }
-
-//             // 関数 block の async 定義
-//             use otel_util::tracing::Instrument;
-//             let execute_async_block = async {
-//                 #block
-//             }.instrument(tracing::info_span!(stringify!(#fn_name)));
-
-//             // 関数 block の async 実行と、panic-catch 部分
-//             use otel_util::tokio::time::{sleep, Duration};
-//             use std::panic::{self, AssertUnwindSafe};
-//             let result = panic::catch_unwind(AssertUnwindSafe(|| {
-//                 tokio::spawn(async move {
-//                     execute_async_block.await.unwrap();
-//                 })
-//             }));
-
-//             let join_result = result.unwrap().await;
-//             sleep(Duration::from_secs(1)).await; // trace の送信の前に、待機しないと、trace が送信されない。
-
-//             if join_result.is_err() {
-//                 panic!("panic occurred");
-//             }
-//         }
-//     };
-//     TokenStream::from(expanded)
-// }
