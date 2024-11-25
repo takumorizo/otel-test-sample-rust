965e6e224610480627eb9b51e7db5ff1031df386
takuya.moriyama <takuya.moriyama@agc.com>
2024-11-26 08:59:17 +0900

    原始的なテストにしようと思う。

--- otel-util/integ_test/src/tests/otel_message_test.rs
+++ otel-util/integ_test/src/tests/otel_message_test.rs
@@ -14,11 +14,11 @@ struct CollectorContainerFactory {
 }
 
 impl CollectorContainerFactory {
-    fn new(base_image: GenericImage, host_config_path: String, host_result_path: String) -> Self {
+    fn new(base_image: GenericImage, host_config_path: &str, host_result_path: &str) -> Self {
         CollectorContainerFactory {
             base_image,
-            host_config_path,
-            host_result_path,
+            host_config_path: host_config_path.to_string(),
+            host_result_path: host_result_path.to_string(),
         }
     }
 
@@ -53,14 +53,15 @@ async fn check_otlp_output_failed_otel_test() {
         .into_owned();
     let config_path = format!("{crate_path}/otel-collector-config.yaml");
     let result_path = format!("{crate_path}/result/{test_name}.json");
+    // let expected_path = format!("{crate_path}/expected/{test_name}.json");
     let file = std::fs::File::create(result_path.clone()).unwrap();
     file.set_permissions(std::fs::Permissions::from_mode(0o666))
         .unwrap();
 
     let collector_factory = CollectorContainerFactory::new(
         GenericImage::new("otel/opentelemetry-collector-contrib", "0.103.1"),
-        config_path,
-        result_path,
+        &config_path,
+        &result_path,
     );
     let _container = collector_factory
         .build()
@@ -78,10 +79,25 @@ async fn check_otlp_output_failed_otel_test() {
     tokio::time::sleep(tokio::time::Duration::from_secs(3)).await;
 
     // then
+    // 元の、otel version　を、0.21.0 から、最新にしないと、opentelemetry_otlp の test と同様のテストが実行できない。
+    // なので、一旦原子的なチェックだけをする。
+
+    // let result_spans =
+    //     read_spans_from_json(std::fs::File::open(result_path).expect("Failed to open result file"));
+
+    // let expected_spans = read_spans_from_json(
+    //     std::fs::File::open(expected_path).expect("Failed to open result file"),
+    // );
+    // print!("spans loaded");
+    // print!("{:?}", result_spans);
+    // print!("{:?}", expected_spans);
+
+    // TraceAsserter::new(result_spans, expected_spans).assert();
+
+    // let a = 1;
 
     unimplemented!("Check the result file");
-
-    // finally
+    // finally: 無くても、コンテナは消える。
     // _container
     //     .stop()
     //     .await
